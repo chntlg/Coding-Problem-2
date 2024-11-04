26159f1e790cdfc6bfbf62336f20919e688f4b98
chntlg <chantalguerrero04@gmail.com>
2024-11-04 22:36:35 +0800

    Initial commit with classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+//User Information Collection
+let subjTitle = "Data Sructures and Algorithm";
+let classSched = "9:30 am - 10:30 am and 1:00 pm - 2:30 pm,Monday and Friday";
+let classRoom = "COR A";
+let classIns = "Dr. Vitruvius John D. Barayuga";
+let studentName = " Chantal G. Guerrero";
+console.log(`${studentName} is currently enrolled in ${subjTitle} with a class schedule of ${classSched} at room ${classRoom}. The instructor for the subject is ${classIns}.`);
